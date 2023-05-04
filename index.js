0e924f62c8ce8e24335e010bbd570d87fbca1a3f
Nestor-Abel-Gonzalez <30273813+Nestor-Abel-Gonzalez@users.noreply.github.com>
2023-05-04 11:58:40 -0300

    2 files modified

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import './style.css';
 
 // Write Javascript code!
 const appDiv = document.getElementById('app');
-appDiv.innerHTML = `<h1>JS Starter</h1>`;
+appDiv.innerHTML = `<h1>Un Gran Comienzo en JS</h1>`;
 
 // Funcion corta para imprimir algo en conssola
 /* esto no es lo mismo eta mal
@@ -17,4 +17,10 @@ function p(message) {
   console.log(message);
 }
 
+let a = '10';
+
 p('hola');
+p('HOLA MUNDO como estan');
+p(5 + 8);
+p(a);
+p(a + 5);
