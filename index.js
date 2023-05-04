d039016ae6b69d7c13fda29efa0900e3986fb376
Nestor-Abel-Gonzalez <30273813+Nestor-Abel-Gonzalez@users.noreply.github.com>
2023-05-04 11:47:46 -0300

    1 file modified

--- index.js
+++ index.js
@@ -4,3 +4,17 @@ import './style.css';
 // Write Javascript code!
 const appDiv = document.getElementById('app');
 appDiv.innerHTML = `<h1>JS Starter</h1>`;
+
+// Funcion corta para imprimir algo en conssola
+/* esto no es lo mismo eta mal
+
+    function print(){
+    console.log()
+}
+*/
+
+function p(message) {
+  console.log(message);
+}
+
+p('hola');
